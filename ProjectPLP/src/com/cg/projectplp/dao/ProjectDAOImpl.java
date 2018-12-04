affacaedf168ed22787b277980c23c0b072129f7
Soumyakarpurapu <44355667+Soumyakarpurapu@users.noreply.github.com>
2018-12-04 13:07:30 +0530

    Add files via upload

---
+++ ProjectPLP/src/com/cg/projectplp/dao/ProjectDAOImpl.java
@@ -0,0 +1,46 @@
+package com.cg.projectplp.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import com.cg.projectplp.beans.Inventory;
+
+@Repository("projectdao")
+public class ProjectDAOImpl implements ProjectDAO{
+	
+	@PersistenceContext
+	EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Inventory> getProductList() {
+		// TODO Auto-generated method stub
+		Query getQuery = em.createQuery("FROM Inventory");
+		List<Inventory> getProduct = getQuery.getResultList();
+		return getProduct;
+	}
+
+	@Override
+	public void addProducts(Inventory inventory) {
+		// TODO Auto-generated method stub
+		
+		em.persist(inventory);
+		em.flush();
+	}
+
+	@Override
+	public Inventory updateProducts(Inventory in) {
+		// TODO Auto-generated method stub
+		Inventory inventory = em.find(Inventory.class, in.getProduct_id());
+		inventory.setPrice(in.getPrice());
+		inventory.setQuantity(in.getQuantity());
+		Inventory inven= em.merge(inventory);
+		return inven;
+	}
+	
+}
